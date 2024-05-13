88c2423baa5b1d483a2a1d3456951c47fd061f80
LDK <gritdrake@gmail.com>
2024-05-13 09:16:15 +0900

    commit

---
+++ L20240513/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main()
+{
+	int number1, number2;
+	std::cin >> number1 >> number2;
+	std::cout << " number1 + number2 = " << number1 + number2 << std::endl;
+	std::cout << " number1 - number2 = " << number1 - number2 << std::endl;
+	std::cout << " number1 * number2 = " << number1 * number2 << std::endl;
+	std::cout << " number1 / number2 = " << number1 / number2 << std::endl;
+}
